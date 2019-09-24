4db505fcf3d75a7d425d5890538f3c5a1d57f528
Felix Hong <felix.hong0321@gmail.com>
2019-09-23 21:57:02 -1000

    Finished wod

---
+++ ziplist.js
@@ -0,0 +1,23 @@
+function zipList(list1, list2) {
+  const zipArray = [];
+  for (let i = 0; i < list1.length; i++) {
+    zipArray.push(list1[i]);
+    zipArray.push(list2[i]);
+  }
+  return zipArray;
+}
+
+const list1 = ['a', 'b', 'c'];
+const list2 = [1, 2, 3];
+
+console.log(zipList(list1, list2));
+
+function zipListTheSimpleWay(list1,list2) {
+  const unflattenArray =  _.zip(list1,list2);
+  return _.flatten(unflattenArray);
+}
+
+const list3 = ['a', 'b', 'c'];
+const list4 = [1, 2, 3];
+
+console.log(zipListTheSimpleWay(list3, list4));
